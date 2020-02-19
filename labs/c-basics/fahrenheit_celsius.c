163b31dfd7eb012b7bc80c4bd6412c9344214fec
Angeljcc102 <angeljcc102@outlook.es>
2020-02-18 20:16:56 -0600

    Solve c-basis lab
    Signed-off-by: Angeljcc102 <angeljcc102@outlook.es>

--- labs/c-basics/fahrenheit_celsius.c
+++ labs/c-basics/fahrenheit_celsius.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #define   LOWER  0       /* lower limit of table */
 #define   UPPER  300     /* upper limit */
@@ -6,12 +7,17 @@
 
 /* print Fahrenheit-Celsius table */
 
-int main()
+int main(int agrc, char **args)
 {
-    int fahr;
+    int fahr = atoi(args[1]);
 
-    for (fahr = LOWER; fahr <= UPPER; fahr = fahr + STEP)
-	printf("Fahrenheit: %3d, Celcius: %6.1f\n", fahr, (5.0/9.0)*(fahr-32));
+    if(agrc == 2){
+      printf("Fahrenheit: %3d, Celcius: %6.1f\n", fahr, (5.0/9.0)*(fahr-32));
+    }
+    if(agrc == 4){
+      for (fahr = atoi(args[1]); fahr <= atoi(args[2]); fahr = fahr + atoi(args[3]))
+      printf("Fahrenheit: %3d, Celcius: %6.1f\n", fahr, (5.0/9.0)*(fahr-32));
+    }
 
     return 0;
 }
---
+++ labs/c-basics/hello.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+	/*printf("First position of argv: %s\n", argv[0]);*/
+	printf("Hello %s World \n", argv[1]);
+	printf("%d \n",argc);
+	return 0;
+}
