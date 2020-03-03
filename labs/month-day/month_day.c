390e69d1dbd152cdd24a25d2c295da7c9f52ec81
Angeljcc102 <angeljcc102@outlook.es>
2020-03-03 15:24:08 -0600

    Solve month-day
    Signed-off-by: Angeljcc102 <angeljcc102@outlook.es>

--- labs/month-day/month_day.c
+++ labs/month-day/month_day.c
@@ -1,8 +1,41 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /* month_day function's prototype*/
-void month_day(int year, int yearday, int *pmonth, int *pday);
+static char daytab[2][13] = {
+    {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31},
+    {0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}
+};
 
-int main() {
+static char *month_name(int *pmonth) {
+	static char *name[] = {"Illegal month","Jan","Feb","Mar","Apr","May",
+			"Jun","Jul","Aug","Sept","Oct","Nov","Dec"};
+	return (*pmonth < 1 || *pmonth > 12) ? name[0] : name[*pmonth];
+}
+
+void month_day(int year, int yearday, int *pmonth, int *pday){
+    int leap = year%4 == 0 && year%100 != 0 || year%400 == 0;
+	if((leap == 0 && yearday > 365) || (leap == 1 && yearday > 366) || year < 0){
+		printf("%s\n", "Invalid year or yearday");
+	}
+	else{
+		int i = 1;
+		pday = &yearday;
+		for(i; *pday > daytab[leap][i]; i++){
+			*pday -= daytab[leap][i];
+		}
+		pmonth = &i;
+		printf("%s %i, %i\n", month_name(pmonth), *pday, year);
+	}
+} 
+
+int main(int agrc, char **args) {
+	if(args[1] == NULL || args[2] == NULL){
+		printf("%s\n", "No year or month written");
+	}
+	else{
+   		int year = atoi(args[1]),yearday = atoi(args[2]),*pmonth, *pday;
+		month_day(year, yearday, pmonth, pday);
+	}
     return 0;
 }
