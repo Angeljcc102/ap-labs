06b00679228025f1edd789d158b1ea8fd9000fbe
Angeljcc102 <angeljcc102@outlook.es>
2020-03-03 15:27:15 -0600

    Solve word-reverser
    Signed-off-by: Angeljcc102 <angeljcc102@outlook.es>

--- labs/word-reverser/reverser.c
+++ labs/word-reverser/reverser.c
@@ -1,6 +1,22 @@
 #include <stdio.h>
 
-int main(){
-    // Place your magic here
-    return 0;
+int main() {
+	char text[500];
+    int c,temp = 0;
+
+	for (int i = 0;(c = getchar()) != EOF && i < 500; i++) {
+        text[i] = c;
+	if(c == '\n'){
+		for(int j = i; j >= temp; j--){
+		 	if(text[j] == '\n'){
+		 	  }
+		 	else{
+		 	printf("%c", text[j]);
+		    }	
+	    }	
+	    printf("%c",'\n');
+	    temp = i;
+		}
+    }
+	return 0;
 }
