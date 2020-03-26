a14be10822d3522dc4f23321dd65b4e6e1a8e875
Angel Cedeño Castro <angeljcc102@outlook.es>
2020-03-25 18:17:03 -0600

    Solve mycat
    Signed-off-by: Angel Cedeño Castro <angeljcc102@outlook.es>

--- labs/mycat/cat2.c
+++ labs/mycat/cat2.c
@@ -1,33 +1,43 @@
 #include <stdio.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#define SIZE 5000000
 
 /* filecopy:  copy file ifp to file ofp */
-void filecopy(FILE *ifp, FILE *ofp)
+void filecopy(int ifp, FILE *ofp)
 {
-    int c;
+    char buffer[SIZE];
+    ssize_t bytes;
 
-    while ((c = getc(ifp)) != EOF)
-        putc(c, ofp);
+    bytes = read(ifp, buffer, SIZE);
+
+    for(int i = 0; i < bytes; i++){
+        putc(buffer[i], ofp);
+    }
 
 }
 
 /* cat:  concatenate files, version 2 */
 int main(int argc, char *argv[])
 {
-    FILE *fp;
-    void filecopy(FILE *, FILE *);
+    int fp;
+    void filecopy(int fp, FILE *);
     char *prog = argv[0];   /* program name for errors */
 
-    if (argc == 1)  /* no args; copy standard input */
-        filecopy(stdin, stdout);
+    if (argc == 1){  /* no args; copy standard input */
+        fprintf(stderr, "no args passed \n");
+    }
     else
         while (--argc > 0)
-            if ((fp = fopen(*++argv, "r")) == NULL) {
+            if ((fp = open(*++argv, O_RDONLY)) == -1) {
                 fprintf(stderr, "%s: can′t open %s\n",
 			prog, *argv);
                 return 1;
             } else {
                 filecopy(fp, stdout);
-                fclose(fp);
+                close(fp);
             }
 
     if (ferror(stdout)) {
