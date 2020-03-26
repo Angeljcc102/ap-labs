aaaf66c1cd49277fbcf7786e9355c40e75158e42
Angel Cedeño Castro <angeljcc102@outlook.es>
2020-03-25 20:22:21 -0600

    Solve str-lib
    Signed-off-by: Angel Cedeño Castro <angeljcc102@outlook.es>

--- labs/str-lib/main.c
+++ labs/str-lib/main.c
@@ -1,5 +1,21 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main() {
-    return 0;
+int mystrlen(char *str);
+char *mystradd(char *origin, char *addition);
+int mystrfind(char *origin, char *substr);
+
+int main(int argc, char* argv[]) {
+
+    if(argc < 4 || argc > 4){
+        printf("Usage: ./main [origin] [addition] [substring] \n");
+        return 0;
+    }
+    
+    int a = 10;
+
+    printf("Initial Lenght      : %d\n",mystrlen(argv[1]));
+    printf("New String          : %s\n",mystradd(argv[1],argv[2]));
+    printf("SubString was found : %s\n",mystrfind(mystradd(argv[1],argv[2]),argv[3])==1? "yes":"no");
+
 }
--- labs/str-lib/strlib.c
+++ labs/str-lib/strlib.c
@@ -1,13 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
 
 
 int mystrlen(char *str){
-    return 0;
+    int len_str = 0;
+    while(*str != '\0'){
+        len_str++;
+        str++;
+    }
+    return len_str;
 }
 
 char *mystradd(char *origin, char *addition){
-    return 0;
+    int len_origin = mystrlen(origin),
+        len_addition = mystrlen(addition),
+        len_new = len_origin+len_addition;
+    char* new_string = (char *)malloc(len_new+1);
+    for(int i = 0; i < len_origin; i++){
+        new_string[i] = origin[i];
+    }
+    for(int j = len_origin; j < len_new; j++){
+        new_string[j] = addition[j-len_origin];
+    }
+    return new_string;
 }
 
 int mystrfind(char *origin, char *substr){
+    int len_origin = mystrlen(origin),
+        len_substr = mystrlen(substr);
+    for(int i = 0; i <= len_origin-len_substr; i++){
+        int j = 0;
+        for(j; j < len_substr; j++){
+            if(origin[i+j] != substr[j]){
+                break;
+            }
+        }
+        if(j == len_substr){
+            return 1;
+        }
+    }
     return 0;
 }
