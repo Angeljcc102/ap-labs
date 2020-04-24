6b58ee803b89a19c05040021eaa00633c88a0652
Angel Cedeño Castro <angeljcc102@outlook.es>
2020-04-23 23:44:38 -0500

    Initial Changes
    Signed-off-by: Angel Cedeño Castro <angeljcc102@outlook.es>

--- labs/clockwall/clock2.go
+++ labs/clockwall/clock2.go
@@ -2,16 +2,19 @@
 package main
 
 import (
+	"fmt"
 	"io"
 	"log"
 	"net"
+	"os"
+	"strings"
 	"time"
 )
 
-func handleConn(c net.Conn) {
+func handleConn(c net.Conn, timezone string, location *time.Location) {
 	defer c.Close()
 	for {
-		_, err := io.WriteString(c, time.Now().Format("15:04:05\n"))
+		_, err := io.WriteString(c, timezone+strings.Repeat(" ", 15-len(timezone))+": "+time.Now().In(location).Format("15:04:05\n"))
 		if err != nil {
 			return // e.g., client disconnected
 		}
@@ -20,7 +23,26 @@ func handleConn(c net.Conn) {
 }
 
 func main() {
-	listener, err := net.Listen("tcp", "localhost:9090")
+
+	if len(os.Args) < 3 || len(os.Args) > 3 {
+		fmt.Println("Usage TZ={Timezone} ./clock2 -port {Port}")
+		os.Exit(1)
+	}
+
+	port := os.Args[2]
+	timezone := os.Getenv("TZ")
+	if len(timezone) == 0 {
+		fmt.Println("Usage TZ={Timezone} ./clock2 -port {Port}")
+		os.Exit(1)
+	}
+
+	listener, err := net.Listen("tcp", "localhost:"+port)
+
+	loc, errt := time.LoadLocation(timezone)
+
+	if errt != nil {
+		log.Panic(timezone + " Is an invalid timezone")
+	}
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -30,6 +52,6 @@ func main() {
 			log.Print(err) // e.g., connection aborted
 			continue
 		}
-		go handleConn(conn) // handle connections concurrently
+		go handleConn(conn, timezone, loc) // handle connections concurrently
 	}
 }
---
+++ labs/clockwall/clockWall.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"time"
+)
+
+func read(response []byte, conn net.Conn, wait chan int) {
+	defer conn.Close()
+	for {
+		_, err := conn.Read(response)
+		if err != nil {
+			log.Fatal("Server disconnection")
+		}
+		fmt.Print(string(response))
+		time.Sleep(1 * time.Second)
+	}
+	wait <- 1
+}
+
+func main() {
+
+	wait := make(chan int)
+	for i := 1; i < len(os.Args); i++ {
+		address := os.Args[i]
+		conn, err := net.DialTimeout("tcp", address, time.Second)
+		if err != nil {
+			log.Fatal(" Invalid host(s) or timeout exceded ")
+		}
+		response := make([]byte, 30)
+
+		go read(response, conn, wait)
+	}
+
+	<-wait
+	close(wait)
+}
